e0e0a605f343d26d262b85045f0363f0f7f83721
daengae <dladmsxor434@naver.com>
2025-08-27 09:18:40 +0900

    feat: 프로젝트 설정

---
+++ src/main/java/com/backsuend/coucommerce/CouCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.backsuend.coucommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouCommerceApplication.class, args);
+	}
+
+}
