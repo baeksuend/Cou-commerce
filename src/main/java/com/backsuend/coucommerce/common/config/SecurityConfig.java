7439f7276c3235aca94798f8989aa94faf58f223
daengae <dladmsxor434@naver.com>
2025-09-09 16:39:51 +0900

    Refactor: CORS 설정 통합 및 상품 API 보안 강화

--- src/main/java/com/backsuend/coucommerce/common/config/SecurityConfig.java
+++ src/main/java/com/backsuend/coucommerce/common/config/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig {
 
 	private static final String[] WHITE_LIST = {
 		"/api/v1/auth/**",    // Auth API
-		"/api/v1/products/**",    // Products API
 		"/uploads/**",    // 이미지 업로드 경로
 		"/static/**",    // 정적 이미지 경로
 		"/swagger-ui/**",     // Swagger UI
